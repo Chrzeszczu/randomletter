35536e3946b91d06adecf4933a47da4bd2e83a88
michal.chrzeszczyk <michal.chrzeszczyk@WRO1-LDL-P10544>
2022-07-12 09:35:38 +0200

    Add project files.

---
+++ randomletter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RandomLetter // Note: actual namespace depends on the project name.
+{
+    public class RanLetterGenerator
+    {
+        public static void Main(string[] args)
+        {
+            string chars = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+            Random rand = new Random();
+            int num = rand.Next(0, chars.Length);
+            Console.WriteLine(chars[num]);
+            string str = Char.ToString(chars[num]);
+            File.WriteAllText(@"C:\Users\michal.chrzeszczyk\Desktop\Jenkins_Workspace\rLetter.txt", str);
+        }
+    }
+}
